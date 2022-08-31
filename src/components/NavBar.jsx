4adedc5d0ba0284b1838b56aa4d1192003fd8052
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2022-08-31 17:00:35 +0200

    feat : stylise navbar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,14 +5,22 @@ import Button from 'react-bootstrap/Button'
 import '../styles/navbar.css'
 import Autenthification from './Autenthification'
 const NavBar = () => {
+  const ClientId = '03880715cb5c42db88b289ec843534bc'
+  const redirection = 'http://localhost:5173/'
+  const outendpoint = 'https://accounts.spotify.com/authorize'
+  const answer = 'token'
+
   return (
     <section className='nav '>
-        <div className='search'>
-            <input type="text" /><BsSearch/>
+        <div className='search ms-4'>
+            <input type="text" className='enter me-2' /><span className='fs-3 text-light'><BsSearch/></span>
         </div>
-        <div className="autenthification">
-            <Button variant="light" >Se connecter</Button>
-            <h5>Dieu-merci</h5>
+        <div className="autenthification me-4 mt-3">
+          <div className='picture'>
+            <span className='profil me-4'>photo</span>
+            <Button variant="primary" className='w-100'> <a href={`${outendpoint}?client_id=${ClientId}&redirect_uri=${redirection}&response_type=${answer}`}>Se connecter</a> </Button>
+          </div> 
+          <h5 className='mt-2 text-light'>Dieu-merci</h5>
         </div>        
     </section>   
   )
