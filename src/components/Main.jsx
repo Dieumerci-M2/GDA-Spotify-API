9f06ad120324e41954b33fb9d22420fb1e511d9d
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2022-08-31 23:26:59 +0200

    feat : add style to music components for good UI design

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import '../styles/main.css'
 import Music from './Music'
+import artiste2 from '../assets/booba.jpg'
+import artiste3 from '../assets/burnaboy.jpg'
+import artiste4 from '../assets/Kirk-franklin.jpg'
+import artiste5 from '../assets/stromae.jpg'
+import artiste1 from '../assets/BenjaminDube.jpg'
+
 const Main = () => {
   return (
     <section>
         <div className="song">
            <h4>Nouvautés</h4> 
-           <Music />
+           <Music artiste1 = {artiste1}/>
         </div>
     </section>
   )
--- src/screens/Principal.jsx
+++ src/screens/Principal.jsx
@@ -4,11 +4,15 @@ import NavBar from '../components/NavBar'
 import Main from '../components/Main'
 import '../styles/principal.css'
 const Principal = () => {
+  const ClientId = '03880715cb5c42db88b289ec843534bc'
+  const redirection = 'http://localhost:5173/'
+  const outendpoint = 'https://accounts.spotify.com/authorize'
+  const answer = 'token'
   return (
     <div className='containe'>
         <SideBar />
         <div className='middle'>
-            <NavBar />
+            <NavBar ClientId = {ClientId} redirection = {redirection} outendpoint = {outendpoint} answer = {answer} />
             <Main />
         </div>
         
