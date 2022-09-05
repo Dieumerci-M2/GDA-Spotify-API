a3a7a0aa616a2e62d3c522af34ccaddb497148be
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2022-09-05 11:06:08 +0200

    feat : show the token on the console

--- src/components/Autenthification.jsx
+++ src/components/Autenthification.jsx
@@ -1,29 +1,40 @@
-import React from 'react'
-import '../styles/autenthification.css'
-import Button from 'react-bootstrap/Button'
+import React from "react";
+import "../styles/autenthification.css";
+import Button from "react-bootstrap/Button";
+import { useEffect, useState } from "react";
 
-const Autenthification = ({ClientId, redirection, outendpoint, answer, token, scope}) => {
+const Autenthification = () => {
+  const ClientId = "03880715cb5c42db88b289ec843534bc";
+  const redirection = "http://localhost:5173/home";
+  const outendpoint = "https://accounts.spotify.com/authorize";
+  //const scope = 'user-read-email playlist-read-private'
+  const answer = "token";
+
   return (
-    <section className='containe text-center'>
-        <div className="contenu">
-          <h1>Bienvenu chez GDA Music</h1>
-          <div className="autenth">
-            <div>
-              <input type="mail" placeholder='E-mail'/>
-            </div>
-            <div>
-              <input type="password" placeholder='pass' />
-            </div>
+    <section className="containe text-center">
+      <div className="contenu">
+        <h1>Bienvenu chez GDA Music</h1>
+        <div className="autenth">
+          <div>
+            <input type="mail" placeholder="E-mail" />
           </div>
-          <div className='connecte'> 
-          { !token?
-              <Button variant="primary" className='w-100'> <a href={`${outendpoint}?client_id=${ClientId}&redirect_uri=${redirection}&response_type=${answer}&scope=${scope}`}>Se connecter</a> </Button>
-              : <Button>Se Déconnecter</Button>
-            }
+          <div>
+            <input type="password" placeholder="pass" />
           </div>
         </div>
+        <div className="connecte">
+              <a
+                href={`${outendpoint}?client_id=${ClientId}&redirect_uri=${redirection}&response_type=${answer}`}
+              >
+                Se connecter
+              </a>
+            
+        
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
+// &scope=${scope}
 
-export default Autenthification
+export default Autenthification;
--- src/screens/Principal.jsx
+++ src/screens/Principal.jsx
@@ -5,24 +5,39 @@ import Main from "../components/Main";
 import "../styles/principal.css";
 import axios from "axios";
 import { useState, useEffect } from "react";
-//import { userContext } from './MyContext'
-const Principal = ({ token }) => {
+const Principal = () => {
   const [searchKey, setSearchKey] = useState("");
   const [artists, setArtists] = useState([]);
 
-  useEffect(() => {
-    (async function () {
-      const { data } = await axios.get(
-        "https://api.spotify.com/v1/search?q=remaster%2520track%3ADoxy%2520artist%3AMiles%2520Davis&type=track%2Cartist&market=ES&limit=10&offset=5",
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-            "Content-Type": "application/json",
-          },
-        }
-      );
-    })();
-  });
+ 
+    // (async function () {
+    //   const { data } = await axios.get(
+    //     "https://api.spotify.com/v1/search?q=remaster%2520track%3ADoxy%2520artist%3AMiles%2520Davis&type=track%2Cartist&market=ES&limit=10&offset=5",
+    //     {
+    //       headers: {
+    //         Authorization: `Bearer ${token}`,
+    //         "Content-Type": "application/json",
+    //       },
+    //     }
+    //   );
+    // })();
+    const [token, setToken] = useState("");
+    useEffect(() => {
+      const hash = window.location.hash
+      let token = window.localStorage.getItem("token")
+      if (!token && hash) {
+        token = hash
+          .substring(1)
+          .split("&")
+          .find((elem) => elem.startsWith("access_token"))
+          .split("=")[1]
+        console.log(token)
+        window.location.hash = ""
+        window.localStorage.setItem("token", token)
+      }
+      setToken(token);
+    }, []);
+ 
 
   //setArtists(data.artists.items)
   return (
